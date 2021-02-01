2ec234a84a31e1c59edbafb73fcfcdb5125cc132
Your Name <you@example.com>
2021-02-01 21:41:33 +0900

    delete cli's vue

--- hamu/components/NavBar.vue
+++ hamu/components/NavBar.vue
@@ -3,6 +3,7 @@
     <nav>
       <nuxt-link to="/">Home</nuxt-link>
       <nuxt-link to="/about">About</nuxt-link>
+      <nuxt-link to="/users/list">List</nuxt-link>
     </nav>
   </div>
 </template>
