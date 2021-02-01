c18f8ff3ce1ef3674408a50dfae911f192cea3a1
Your Name <you@example.com>
2021-02-01 22:02:12 +0900

    error  Expected '===' and instead saw '=='

---
+++ hamu/pages/users/_id.vue
@@ -0,0 +1,32 @@
+<template>
+  <h1>ユーザID: {{ user.name }}</h1>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      id: this.$route.params.id,
+      users: [
+        {
+          id: 1,
+          name: 'John Doe',
+        },
+        {
+          id: 2,
+          name: 'Kevin Smith',
+        },
+        {
+          id: 3,
+          name: 'Harry Bosch',
+        },
+      ],
+    }
+  },
+  computed: {
+    user() {
+      return this.users.find((user) => user.id === this.id)
+    },
+  },
+}
+</script>
