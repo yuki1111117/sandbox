1eec665c1e70436e2b99761f1b49203450463cec
Your Name <you@example.com>
2021-02-03 21:38:52 +0900

    change log to hamu.jpg

--- cli/src/App.vue
+++ cli/src/App.vue
@@ -41,6 +41,12 @@
         <v-card-title>
           <h1 class="display-1">ログイン</h1>
         </v-card-title>
+        <v-card-text>
+          <v-form>
+            <v-text-field label="ユーザ名" />
+            <v-text-field label="パスワード" />
+          </v-form>
+        </v-card-text>
       </v-card>
     </v-app>
   </v-app>
