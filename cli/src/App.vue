f44e7f6b3b71a8958081a95b99ef706ef44e4a5b
yuki1111117 <tokuakeyuki@gmail.com>
2021-02-06 12:44:59 +0900

    Setting HeadNavBar

--- cli/src/App.vue
+++ cli/src/App.vue
@@ -1,9 +1,28 @@
 <template>
   <v-app>
     <!-- LeftNavBar -->
-    <v-navigation-drawer app v-model="drawer" clipped
-      >ナビだからよ</v-navigation-drawer
-    >
+    <v-navigation-drawer app v-model="drawer" clipped>
+      <v-container>
+        <v-list-item>
+          <v-list-item-content>
+            <v-list-item-title class="title grey--text text--darken-2">
+              はむのリストだからよ
+            </v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+        <v-divider></v-divider>
+        <v-list dense nav>
+          <v-list-item v-for="nav_list in nav_lists" :key="nav_list.name">
+            <v-list-item-icon>
+              <v-icon>{{ nav_list.icon }}</v-icon>
+            </v-list-item-icon>
+            <v-list-item-content>
+              <v-list-item-title>{{ nav_list.name }}</v-list-item-title>
+            </v-list-item-content>
+          </v-list-item>
+        </v-list>
+      </v-container>
+    </v-navigation-drawer>
 
     <!-- HeadNavBar -->
     <v-app-bar
@@ -32,6 +51,48 @@
           width="18"
         />
       </div>
+      <v-toolbar-items>
+        <v-btn text>File</v-btn>
+        <v-menu offset-y>
+          <template v-slot:activator="{ on }">
+            <v-btn v-on="on" text>Edit</v-btn>
+          </template>
+          <v-list>
+            <v-list-item>
+              <v-list-item-content>
+                <v-list-item-title>Consulting and support</v-list-item-title>
+              </v-list-item-content>
+            </v-list-item>
+            <v-list-item>
+              <v-list-item-content>
+                <v-list-item-title>Discord community</v-list-item-title>
+              </v-list-item-content>
+            </v-list-item>
+          </v-list>
+        </v-menu>
+        <v-menu offset-y>
+          <template v-slot:activator="{ on }">
+            <v-btn v-on="on" text>Help</v-btn>
+          </template>
+          <v-list>
+            <v-list-item>
+              <v-list-item-content>
+                <v-list-item-title
+                  >Githubだからよ 見ててってくれよな</v-list-item-title
+                >
+              </v-list-item-content>
+            </v-list-item>
+            <v-list-item>
+              <v-list-item-content>
+                <v-list-item-title
+                  >作りたてだからよ 勘弁してくれよな</v-list-item-title
+                >
+              </v-list-item-content>
+            </v-list-item>
+          </v-list>
+        </v-menu>
+      </v-toolbar-items>
+      <v-spacer></v-spacer>
       <v-toolbar-title>はむのハウスだからよ</v-toolbar-title>
       <v-spacer></v-spacer>
       <v-btn href="https://github.com/yuki1111117/sandbox" target="_blank" text>
@@ -63,7 +124,7 @@
 
     <v-footer absolute app dark>
       <v-col class="" cols="12">
-        {{ new Date().getFullYear() }} — <strong>はむのハウス</strong>
+        {{ new Date().getFullYear() }} — <strong>はむさのハウス</strong>
       </v-col></v-footer
     >
   </v-app>
@@ -71,5 +132,9 @@
 
 <script>
 import HelloWorld from "./components/HelloWorld"; export default { name: "App",
-components: { HelloWorld }, data() { return { drawer: null }; } };
+components: { HelloWorld }, data() { return { drawer: null, nav_lists: [ { name:
+"Getting Started", icon: "mdi-vuetify" }, { name: "ユーザー設定", icon:
+"mdi-cogs" }, { name: "Styles & animations", icon: "mdi-palette" }, { name:
+"UIComponents", icon: "mdi-view-dashboard" }, { name: "Directives", icon:
+"mdi-function" }, { name: "Preminum themes", icon: "mdi-vuetify" } ] }; } };
 </script>
