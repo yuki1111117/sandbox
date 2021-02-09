2b35187ea961f11c53107fb71573f1d85ce934ca
yuki1111117 <tokuakeyuki@gmail.com>
2021-02-09 16:11:08 +0900

    delete cli

--- hamu/nuxt.config.js
+++ hamu/nuxt.config.js
@@ -3,8 +3,8 @@ import colors from 'vuetify/es5/util/colors'
 export default {
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    titleTemplate: '%s - hamu',
-    title: 'hamu',
+    titleTemplate: '%s - hamu house',
+    title: 'hamu house',
     htmlAttrs: {
       lang: 'en',
     },
