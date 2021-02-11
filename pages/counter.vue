d95bf1d3790130436cbca90e0e83b5ceea1d9cdb
yuki <tokuakeyuki@gmail.com>
2021-02-11 06:27:07 +0000

    Used mapXXX from vuex

--- pages/counter.vue
+++ pages/counter.vue
@@ -3,21 +3,17 @@
     <h1>Counter</h1>
     <marquee>クッキー増やすからよ</marquee>
     <p>
-      <button @click="increment">不思議なぽっけだからよ</button>
+      <button @click="incrementOne">不思議なぽっけだからよ</button>
     </p>
     <h1>クッキーが{{ count }}枚あるからよ</h1>
   </div>
 </template>
 
 <script>
-import { mapState } from 'vuex'
+import { mapState, mapActions } from 'vuex'
 
 export default {
   computed: mapState(['count']),
-  methods: {
-    increment() {
-      this.$store.dispatch('incrementOne')
-    },
-  },
+  methods: mapActions(['incrementOne']),
 }
 </script>
