43b1b762ccb6073b741f99a77a38b1ee537334ab
yuki <tokuakeyuki@gmail.com>
2021-03-07 08:26:37 +0000

    Add Text-field on setting

--- pages/setting/index.vue
+++ pages/setting/index.vue
@@ -1,16 +1,20 @@
 <template>
   <v-row>
-    <v-col cols="12" sm="3" md="3" lg="4" xl="5">
-      <v-btn></v-btn>
+    <v-col cols="12" sm="12" md="12" lg="4" xl="4">
+      <v-text-field label="Title"></v-text-field>
+      <v-text-field label="SortBy"></v-text-field>
+      <v-text-field label="ItemPerPage" type="Number"></v-text-field>
     </v-col>
     <ChatRanking
       title="チャットするとこ"
       sortBy="time"
       itemPerPage="3"
     ></ChatRanking>
-    <v-col cols="12" sm="3" md="3" lg="4" xl="4">
-      <v-btn></v-btn>
-    </v-col>
+    <ChatRanking
+      title="チャットするとこ"
+      sortBy="time"
+      itemPerPage="3"
+    ></ChatRanking>
   </v-row>
 </template>
 
