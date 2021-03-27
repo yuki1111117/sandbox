15689766c80698a332771731fd322634522dd9cb
yuki1111117 <35645877+yuki1111117@users.noreply.github.com>
2021-03-27 03:57:35 +0000

    SET jum Btn in search _id page

--- pages/search/_id.vue
+++ pages/search/_id.vue
@@ -12,16 +12,6 @@
         multiple
         small-chips
       >
-        <template #no-data>
-          <v-list-item>
-            <v-list-item-content>
-              <v-list-item-title>
-                No results matching "<strong>{{ search }}</strong
-                >". Press <kbd>enter</kbd> to create a new one
-              </v-list-item-title>
-            </v-list-item-content>
-          </v-list-item>
-        </template>
       </v-combobox>
       {{ model }}
     </v-container>
@@ -43,7 +33,14 @@ export default {
     }
   },
   data() {
-    return { urlId: this.$route.params.id }
+    return {
+      urlId: this.$route.params.id,
+      // combobox
+      model: [],
+      engine: 'https://duckduckgo.com/',
+      items: [],
+      // combobox END
+    }
   },
   mounted() {
     if (this.urlId) {
@@ -54,8 +51,46 @@ export default {
         .on('value', (snapshot) => (this.remoteData = snapshot.val()))
     }
   },
+
   updated() {
     this.remoteData.key = this.urlId
+  },
+
+  methods: {
+    jump() {
+      if (this.model === []) {
+        return
+      }
+      // creat url
+      const head = 'q='
+      let q = ''
+      let url = ''
+      this.model.forEach((e) => {
+        q = q.concat(e).concat('+')
+      })
+      q = q.slice(0, -1)
+      url = head.concat(q)
+      const link = this.engine.concat('?').concat(url)
+      // create url END
+      const cardKey = firebase
+        .database()
+        .ref('search')
+        .child(this.urlId)
+        .child('then')
+        .push().key
+      firebase
+        .database()
+        .ref('search')
+        .child(this.urlId)
+        .child('then')
+        .child(q)
+        .set({
+          keyword: q,
+          key: cardKey,
+          createdAt: firebase.database.ServerValue.TIMESTAMP,
+        })
+      location.assign(link)
+    },
   },
 }
 </script>
