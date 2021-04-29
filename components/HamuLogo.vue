78724a2cab429f64749d49d2674e1c8defcb0b29
yuki <tokuakeyuki@gmail.com>
2021-04-29 12:11:08 +0000

    Hamu logo change move

--- components/HamuLogo.vue
+++ components/HamuLogo.vue
@@ -6,13 +6,5 @@
 .HamuLogo {
   height: 180px;
   width: 180px;
-  transform: rotateY(560deg);
-  animation: turn 3.5s ease-out forwards 1s;
-}
-
-@keyframes turn {
-  100% {
-    transform: rotateY(0deg);
-  }
 }
 </style>
